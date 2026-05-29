ae0c9c61bb6c86a54a820a109a175ca5dd6cb4dd
babaanefrillkathlene-svg <babaanefrillkathlene@gmail.com>
2026-05-29 12:56:17 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "123456",
+  appId: "APP_ID"
+};
